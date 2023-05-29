95dc28d3db4b827ad469b35b33f64c249482b338
ben <bouduben@gmail.com>
2023-05-29 16:25:12 +0200

    feat : gonfig file with dotenv

--- app/database.py
+++ app/database.py
@@ -1,9 +1,17 @@
+from functools import lru_cache
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:MonSuperPassePostgres@localhost/agoraguet-fa"
+from app import config
+
+
+load_dotenv()
+settings = config.Settings()
+print("-------------------->settings", settings)
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_host}/{settings.database_name}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
